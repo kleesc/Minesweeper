ff3ca24413a1f171649c955b52ac418f9fa517a7
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2014-11-29 18:00:51 -0500

    update

--- MineSweeper.java
+++ MineSweeper.java
@@ -11,13 +11,7 @@ public class MineSweeper extends JFrame {
     private MinePane grid;
 
     public MineSweeper(int rows, int columns, int mines) {
-        setSize(800, 600);
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        /* Menu */
-        JMenuBar menuBar = new JMenuBar();
-        //JMenu 
+        init();
 
         game = new MineSweeperGame();
         info = new InfoPane(game);
@@ -28,36 +22,7 @@ public class MineSweeper extends JFrame {
     }
 
     public MineSweeper() {
-        setSize(800, 600);
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        /* Menu */
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Menu");
-        JMenuItem itm = new JMenuItem("Save");
-        itm.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    getContentPane().remove(grid);
-                    getContentPane().invalidate();
-                    getContentPane().add(grid = new MinePane(new MineSweeperGame(30,30)));
-                    getContentPane().revalidate();
-                }
-            });
-        menu.add(itm);
-        itm = new JMenuItem("Load");
-        // Add Action listener to item
-        menu.add(itm);
-        itm = new JMenuItem("Options");
-        // Add ActionListener to item
-        menu.add(itm);
-        itm = new JMenuItem("Exit");
-        // Add ActionListener to item
-        menu.add(itm);
-        menuBar.add(menu);
-
-        setJMenuBar(menuBar);
-        
+        init();
 
         game = new MineSweeperGame();
         info = new InfoPane(game);
@@ -65,6 +30,93 @@ public class MineSweeper extends JFrame {
 
         getContentPane().add(grid);
         getContentPane().add(info, BorderLayout.EAST);
+    }
+
+    public void init() {
+        setSize(800, 600);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* Menu */
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+
+        /* New Game */
+        JMenuItem itm = new JMenuItem("New Game");
+        itm.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    int rows, columns, mines = 0;
+
+                    JPanel textPane = new JPanel();
+                    JTextField field1 = new JTextField(3);  
+                    textPane.add(field1);
+                    JTextField field2 = new JTextField(3);
+                    textPane.add(field2);
+                    JTextField field3 = new JTextField(3);
+                    textPane.add(field3);
+
+                    boolean valid = false;
+                    do {
+                        int ok = JOptionPane.showConfirmDialog(null, textPane, 
+                                                               "New Game: Current game will be lost",
+                                                               JOptionPane.OK_CANCEL_OPTION);
+                        if(ok == JOptionPane.OK_OPTION) {
+                            try {
+                                rows = Integer.parseInt(field1.getText());
+                                columns = Integer.parseInt(field2.getText());
+                                mines = Integer.parseInt(field3.getText());
+                                valid = true;
+
+                                getContentPane().remove(grid);
+                                getContentPane().invalidate();
+                                getContentPane().add(grid = new MinePane(new MineSweeperGame(30,30)));
+                                getContentPane().revalidate();
+                                
+                            } catch (NumberFormatException ex) {
+                                JOptionPane.showMessageDialog(null, "Enter 3 Integers");
+                            }
+                        } else {
+                            break;
+                        }
+                    } while(!valid);
+                }
+            });
+        menu.add(itm);
+
+        /* Save */
+        itm = new JMenuItem("Save");
+        itm.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    
+                }
+            });
+        menu.add(itm);
+
+        /* Load */
+        itm = new JMenuItem("Load");
+        itm.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    
+                }
+            });
+        menu.add(itm);
+
+        /* Exit */
+        itm = new JMenuItem("Exit");
+        itm.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(JOptionPane.showConfirmDialog(null,
+                                                     "Are you sure? Current game will be lost.", 
+                                                     "Exit", JOptionPane.YES_NO_OPTION) == 0) {
+                        System.exit(0);
+                    }
+                }
+            });
+        // Add ActionListener to item
+        menu.add(itm);
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
     }
 
     public void run() {   
