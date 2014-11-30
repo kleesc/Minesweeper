cd834b14ae1bd073b480375631d317a86828c309
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2014-11-30 05:57:52 -0500

    Added a cleaner solution for getting values and position from buttons

--- MinePane.java
+++ MinePane.java
@@ -5,67 +5,64 @@ import java.awt.event.*;
 import java.io.*;
 
 public class MinePane extends JPanel implements Serializable {
-    private JButton grid[][];
+    private CustomButton grid[][];
     private MineSweeperGame currentGame;
-    private int rows;
-    private int columns;
-    private boolean pressed[][];
 
     public MinePane(MineSweeperGame currentGame) {
-        this.currentGame = currentGame;
-        rows = currentGame.getRows();
-        columns = currentGame.getColumns();
-        pressed = new boolean[rows][columns];
+        int rows = currentGame.getRows(), columns = currentGame.getColumns();
+        
         setLayout(new GridLayout(rows, columns));
+        this.currentGame = currentGame;
 
         int mineField[][] = currentGame.getMineField();
-        grid = new JButton[rows][columns];
+        grid = new CustomButton[rows][columns];
+
         for(int i = 0; i < mineField.length; i++) {
             for(int j = 0; j < mineField[i].length; j++) {
-                grid[i][j] = new JButton();
-                grid[i][j].addActionListener(new ButtonClick(currentGame, i, j));
+                grid[i][j] = new CustomButton(i, j, mineField[i][j]);
+                grid[i][j].addActionListener(new ButtonClick());
                 add(grid[i][j]);
             }
         }
     }
     
     public class ButtonClick implements ActionListener {
-    	private int i;
-    	private int j;
-    	
-    	public ButtonClick(MineSweeperGame currentGame, int i, int j) {
-    		this.i = i;
-    		this.j = j;
-    	}
     	
         public void actionPerformed(ActionEvent e) {
             System.out.println("test");
             
             if(e.getSource() instanceof JButton) {
                 JButton btn = (JButton) e.getSource();
-                btn.setBackground(Color.RED);
-                btn.setText(Integer.toString(currentGame.getSquare(i, j)));
-                if(currentGame.getSquare(i, j) == 0 && pressed[i][j] == false) {
-                	pressed[i][j] = true;
-                	if(i + 1 < rows)
-                		grid[i + 1][j].doClick();
-                	if(j + 1 < columns)
-                		grid[i][j + 1].doClick();
-                	if(i - 1 >= 0)
-                		grid[i - 1][j].doClick();
-                	if(j - 1 >= 0)
-                		grid[i][j - 1].doClick();
-                	if(i + 1 < rows && j + 1 < columns)
-                		grid[i + 1][j + 1].doClick();
-                	if(i + 1 < rows && j - 1 >= 0)
-                		grid[i + 1][j - 1].doClick();
-                	if(i - 1 >= 0 && j + 1 < columns)
-                		grid[i - 1][j + 1].doClick();
-                	if(i - 1 >= 0 && j - 1 >= 0)
-                		grid[i - 1][j - 1].doClick();
-                }
-                pressed[i][j] = true;
+                btn.setBackground(Color.GREEN);
             }
+        }
+    }
+
+    private class CustomButton extends JButton {
+        /* 
+         * Do NOT name getters getX and getY because it overrides the parent's Component members
+         * Causing the button to get stacked on the panel
+         * Wasted too much time before realizing... 
+         */
+        private int x, y, value;
+        
+        public CustomButton(int x, int y, int value) {
+            super();
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+
+        public int getXPos() {
+            return this.x;
+        }
+
+        public int getYPos() {
+            return this.y;
+        }
+
+        public int getValue() {
+            return this.value;
         }
     }
 }
